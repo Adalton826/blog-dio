20d4834b5602fabe98742c58ccc52bf6d4fb7161
adalton Passarelli <adalton.passarelli@fatec.sp.gov.br>
2024-06-02 09:29:08 -0300

    compenetes

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string="https://th.bing.com/th/id/OIP.Y5dsZfsTFldh1jgIjqiOPAHaEK?w=291&h=180&c=7&r=0&o=5&pid=1.7" 
+  @Input()
+  cardTitle: string="novo filme da viuva negra"
+  
+
+  constructor (){}
+  ngOnInit(): void {
+
+  }
 
 }
